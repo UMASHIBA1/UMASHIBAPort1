ca499427e313bd01e5b35960d746a5d1ac51e2f1
UMASHIBA1 <umashiba1@gmail.com>
2019-05-22 01:35:48 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/MyPortFolio1/precache-manifest.63a16e2fc3bde753a89c9107e2adc80d.js"
+  "/MyPortFolio1/precache-manifest.d1e0702799556636c1b66a79af84a7c0.js"
 );
 
 self.addEventListener('message', (event) => {
